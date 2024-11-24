ab588c7b8fd2bce98b79750676dcbde0982d498e
mohamedsaeed138 <mohamed.saeed.goda.13@gmail.com>
2024-11-24 19:24:21 +0200

    added testing_datasets

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from app.etl.controllers import *
 
 # for testing the program wihtout ui
 if __name__ == "__main__":
-    query = "select * from [csv:E:/College/Year 4/Term 1/CS461 Intelligent Systems - Dr.Ahmed Elshewey/Section Assignments/03- Team_09_Ensemble_Models_Boosting/hotel_bookings.csv];"
+
+    query = "select * from [csv:testing_datasets/hotel_bookings.csv];"
     compilation_result = compile_to_python(query)
     if type(compilation_result) == Success:
         python_code = compilation_result.unwrap()
@@ -12,6 +13,7 @@ if __name__ == "__main__":
             print(execution_result.unwrap())
         else:
             print(execution_result.unwrap())
+
     # to print failure string value
     else:
         print(compilation_result.unwrap())
