274610c135b9bf518b2cc3bb4de8953f9c458c5a
mohamedsaeed138 <mohamed.saeed.goda.13@gmail.com>
2024-11-24 17:55:56 +0200

    refactor etl.controllers and decpuple it from the ui and remove from it ui mutation repsonsibilites

--- app/etl/controllers.py
+++ app/etl/controllers.py
@@ -1,51 +1,57 @@
-import traceback
-
 from app.compiler import parser
-from app.gui import ui
+from returns.result import Success, Failure
+from typing import Union
+import traceback
+from pandas import DataFrame
 
-from datetime import datetime
-import time
-from tabulate import tabulate
 
+def compile_to_python(query: str) -> Union[Success[str], Failure[str]]:
+    """
+    Compiles a SQL-like query string into corresponding Python code or returns an error string if it fails to parse the query.
 
-def compile():
+    Args:
+          query (str): The SQL-like query to be compiled. For example:
+            - A successful query: "SELECT * FROM [csv:data/players.csv] WHERE age > 30;"
+            - A query with an error: "SELECT * FROM WHERE age > 30"
+    Returns:
+        Union[Success[str], Failure[str]]: A `Success` monad containing the compiled Python code if parsing is successful,
+                                           or a `Failure` monad containing the error string if parsing fails.
+    """
     try:
-        query = str(ui.inputbox.text())
-        result = parser.parse(query)
-        ui.outputbox.setText(str(result))
-    except Exception as e:
-        traceback.print_exc()
-        # print("Compilation Error.", e)
+        # Assuming parser.parse() is some parsing logic for the SQL-like query
+        return Success(str(parser.parse(query)))  # type: ignore
+    except Exception:
+        # Return a Failure monad containing the stack trace in case of an error
+        return Failure(traceback.format_exc())
 
 
-def execute():
+def execute_python_code(python_code: str) -> Union[Success[DataFrame], Failure[str]]:
+    """
+    Executes the given Python code and returns the resulting transformed data as a `DataFrame`,
+    or an error message in case of failure.
+
+    Args:
+        python_code (str): The Python code to be executed as a string. The code should perform
+                           transformations on data that result in a variable named `transformed_data`
+                           being created, which will be returned as a `DataFrame` on success.
+                             Example of a valid `python_code` argument:
+                           ```python
+
+                           import pandas as pd
+                           data = {'name': ['Alice', 'Bob'], 'age': [30, 25]}
+                           transformed_data = pd.DataFrame(data)
+
+                           ```
+    Returns:
+        Union[Success[DataFrame], Failure[str]]:
+            - A `Success` monad containing a `DataFrame` of the transformed data if the code executes successfully.
+            - A `Failure` monad containing a string with the error message and stack trace if execution fails.
+    """
     try:
-        current_time = datetime.now().strftime("%H:%M:%S")
-        start_time = time.time()
-        code = str(ui.outputbox.toPlainText())
-        exec(str(code))
-        ui.results.setText(f"Execution started at: {current_time}\n")
+        exec(python_code)
+        from app.etl.core import transformed_data
 
-        from app.etl import core
+        return Success(transformed_data)
 
-        total = time.time() - start_time
-        mins = int(total / 60)
-        secs = float(total % 60)
-        ui.results.setText(
-            ui.results.toPlainText()
-            + f"\nExcecution process on {len(core.transformed_data)} rows.\n \tTook: {mins} Minutes, {secs:.2f} Seconds.\n"
-        )
-
-        if isinstance(core.transformed_data, str):
-            ui.results.setText(
-                ui.results.toPlainText() + f"\n{core.transformed_data}\n"
-            )
-        else:
-            table = tabulate(
-                core.transformed_data, headers=core.transformed_data.keys()
-            )
-            ui.results.setText(ui.results.toPlainText() + f"\n{table}\n")
-
-    except Exception as e:
-        traceback.print_exc()
-        # print("Execution Error.", e.)
+    except Exception:
+        return Failure(traceback.format_exc())
